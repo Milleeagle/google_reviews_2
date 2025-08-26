b855d4a899bc0d278030e339109cfb63e9aae798
emilj <emilj@WORKSTATION>
2025-08-26 16:13:01 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using google_reviews.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace google_reviews.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
